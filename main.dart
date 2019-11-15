dc491aa130065084f21010d366d654a62d9de52c
Nabanita Sarkar <36895154+nabanita-sarkar@users.noreply.github.com>
2019-11-16 01:00:58 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+void main() => runApp(MyApp());
+
